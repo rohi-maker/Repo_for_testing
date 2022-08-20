35fced731b60269bcd2ee8f186355dc6cd884e21
Shivay <sanwalrohit333@gmail.com>
2022-08-20 18:23:55 +0530

    LOGIN SYSTEM ADDED

--- Second.java
+++ Second.java
@@ -2,7 +2,11 @@
 interface MyInterface{ //* THIS IS FUNCTIONAL INTERFACE 
     public int myMethode(int a,int b);
 }
-
+class LoginSystem{
+    public void login(){
+        System.out.println("THE LOGIN IS SUCCESSFULL");
+    }
+}
 public class Second {
     public static void main(String[] args) {
         MyInterface mi=(int a,int b)->{return (a+b);};

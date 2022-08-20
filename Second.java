cd5692e7f56c435b5102ec56e14871e3a7adac85
Shivay <sanwalrohit333@gmail.com>
2022-08-20 18:20:11 +0530

    INITIAL COMMIT

---
+++ Second.java
@@ -0,0 +1,11 @@
+@FunctionalInterface
+interface MyInterface{ //* THIS IS FUNCTIONAL INTERFACE 
+    public int myMethode(int a,int b);
+}
+
+public class Second {
+    public static void main(String[] args) {
+        MyInterface mi=(int a,int b)->{return (a+b);};
+        System.out.println(mi.myMethode(5, 6));
+    }
+}
